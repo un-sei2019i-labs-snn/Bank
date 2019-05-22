5bd45f23c5939f0617c01b850f848fa787fe718d
SantiMahecha <smahechap@unal.edu.co>
2019-05-22 15:30:24 -0500

    funcion suma creada por SantiagoM

---
+++ test/Lab3.java
@@ -0,0 +1,24 @@
+package lab3;
+
+import java.util.Scanner;
+
+
+public class Lab3 {
+
+public static int suma(int a,int b,int c ,int d){        
+    return a+b+c+d;
+}
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+    
+int c =0;
+        c = suma(3,7,6,34);
+        
+        System.out.println(c);        
+
+        
+    }
+    
+} 
